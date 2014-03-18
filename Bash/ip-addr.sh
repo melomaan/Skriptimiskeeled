d226a091853d2213869fd08d381276617ea7bfd0
Üllar Seerme <ullar.seerme@itcollege.ee>
2014-03-18 16:35:30 +0200

    Translated the script into English.

--- Bash/ip-addr.sh
+++ Bash/ip-addr.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Üllar Seerme, A21
-# Skript saab sisendiks logifaili, kus on IP-aadressid ning tagastab sealt unikaalsed aadressid, mille nimelahendus õnnestus.
+# Üllar Seerme
+# Script gets a log file as an input and returns all unique IP-addresses for which it was able to resolve the hostnames.
 export LC_ALL=C
 
 ARG=$1
 ACT=$(grep NEVE "$ARG" | cut -d " " -f1 | sort -u)
 for i in $ACT; do
     echo -n "$i - "
-    host $i > /dev/null && host $i | cut -d " " -f5 || echo "Tekkis viga"
+    host $i > /dev/null && host $i | cut -d " " -f5 || echo "An error occurred!"
 done
