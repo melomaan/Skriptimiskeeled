e4b62025b6d046b4c97e2d6188012e5d50114ecd
Üllar Seerme <ullar.seerme@itcollege.ee>
2014-03-25 16:05:42 +0200

    Changed docstrings to PEP 257. Added description.

--- Python/uploader.py
+++ Python/uploader.py
@@ -1,23 +1,28 @@
+"""Script for performing synchronous uploads to one of many
+ZippyShare.com upload domains. Takes user's input for file
+location (absolute path) and returns the download link.
+"""
+
 import sys
 import random
 import re
-import chilkat
+try:
+    import chilkat
+except ImportError:
+    print("Error importing chilkat. Install module from "
+          "http://www.chilkatsoft.com/python.asp")
 
 
 class Uploader():
-    # Define variables for the various HTTP classes
     http = chilkat.CkHttp()
     req = chilkat.CkHttpRequest()
     req.UseUpload()
 
     def __init__(self):
-        # Default upload URL of the main domain to be used
+        """Define the default upload URL of the main domain in use."""
         self.req.put_Path("/upload")
         self.domain = ""
         loc = input("Enter the location of what you want to upload: ")
-        # First argument of the AddFileForUpload method is an arbitrary name,
-        # which in HTML is the form field name of the input tag. Method
-        # returns a boolean
         add = self.req.AddFileForUpload("file1", loc)
 
         if add is not True:
@@ -25,22 +30,23 @@ class Uploader():
             sys.exit()
 
         self.unlock_chilkat()
-        self.get_prefix()
+        self.rand_domain()
         self.upload()
 
     def unlock_chilkat(self):
-        # The following unlocks the Chilkat module, which is necessary because
-        # it is commercially licensed. This should be called once at the
-        # beginning of the program
+        """Call once at the beginning to unlock the commercially licensed
+        chilkat module. Any string will do as argument.
+        """
         unlock = self.http.UnlockComponent("Unlock")
 
         if unlock is not True:
             print(self.http.lastErrorText())
             sys.exit()
 
-    def get_prefix(self):
-        # ZippyShare has multiple upload domains ranging from www1 to www78,
-        # so each upload gets a random domain for uploading
+    def rand_domain(self):
+        """Choose a random upload domain from range 1 to 78. This is
+        specific to ZippyShare.
+        """
         prefix = "www"
         arr = []
         for i in range(1, 79):
@@ -50,26 +56,25 @@ class Uploader():
         self.domain = "%s.zippyshare.com" % rand
 
     def upload(self):
+        """Begin synchronous upload and use re module (regex) to find the
+        resource locator in the body of the response.
+        """
         port = 80
         ssl = False
         resp = self.http.SynchronousRequest(self.domain, port, ssl, self.req)
 
-        # If there is no response, then show the error text
         if resp is None:
             print(self.http.lastErrorText())
         else:
-            # Use regex to find the upload domain plus the locator for the file
-            # in the body
             matcher = re.search(self.domain + "/v/.*html", resp.bodyStr())
             if matcher:
-                # Return the entire match
                 print("Link:", matcher.group(0))
             else:
                 print("No match was found in the body of the source.")
 
 
 def main():
-    ifh = Uploader()
+    Uploader()
 
 if __name__ == "__main__":
     main()
