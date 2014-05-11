fb3f5aa738af04a8b695ffc0b050c1395ceacb0d
melomaan <ullar.seerme@itcollege.ee>
2014-05-11 15:18:40 +0300

    Trim explanation

--- Bash/version-check.sh
+++ Bash/version-check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Üllar Seerme
-# Script checks which version of a package is installed and only displays the bare essentials. If the package isn't installed, then it returns "Installed: (None)".
+# Script checks which version of a package is installed and only displays the bare essentials. 
+# If the package isn't installed, then it returns "Installed: (None)".
 export LC_ALL=C
 
 ARG=$1
