ce02ee2f73e47253f23e4bee656da087bc5f707d
Üllar Seerme <ullar.seerme@itcollege.ee>
2014-05-13 15:17:34 +0300

    Asks about buying an elephant ad nauseam

---
+++ PowerShell/elephant_loop.ps1
@@ -0,0 +1,13 @@
+﻿# Script loops indefinitely asking about purchasing an elephant
+# without any regard to what the user inputs. This was our first
+# attempt at using PowerShell
+
+$foo = "Wanna buy an elephant?"
+$bar = "(Yes/No)"
+Write-Host $foo
+
+while ($true)
+{
+    $ans = Read-Host "$bar"
+    Write-Host "Everybody says $ans, but do you" $foo.ToLower()
+}
