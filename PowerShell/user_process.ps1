f77e925d41f3b63232e5401f8fa83bad128c3e42
Üllar Seerme <ullar.seerme@itcollege.ee>
2014-05-22 23:26:59 +0300

    Retrieve process list of given user

---
+++ PowerShell/user_process.ps1
@@ -0,0 +1,11 @@
+﻿param($user)
+$processes = Get-Process -IncludeUserName | ? { $_.UserName -eq $user } | Format-Table -AutoSize Id, UserName, ProcessName
+
+if ($processes.length -eq 0) {
+    Write-Host "No such user. Defaulting to current user."
+    $user = "$env:USERDOMAIN\$env:USERNAME"
+    # Doesn't yet show process list of current user
+    Write-Host $user
+} else {
+    $processes
+}
