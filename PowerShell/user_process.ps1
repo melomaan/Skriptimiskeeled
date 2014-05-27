bc9eb0b6119e413fb78bebde13c3588edebdd12e
Üllar <yates@protonmail.ch>
2014-05-27 20:28:29 +0300

    Update user_process.ps1

--- PowerShell/user_process.ps1
+++ PowerShell/user_process.ps1
@@ -3,6 +3,7 @@
  * returns the list of processes belonging to that certain set. Can also
  * be left blank and then the current user will be used.
  */
+ 
 param($user = "$env:USERDOMAIN\$env:USERNAME")
 Get-Process -IncludeUserName | ? { $_.UserName -eq $user } | Format-Table
 -AutoSize Id, UserName, ProcessName
