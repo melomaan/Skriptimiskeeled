e5bd6e462e3c998344bf2885a941bcd7e7e3aa99
Üllar Seerme <ullar.seerme@itcollege.ee>
2014-06-14 16:13:07 +0300

    Updated comment

--- PowerShell/elephant_loop.ps1
+++ PowerShell/elephant_loop.ps1
@@ -1,8 +1,8 @@
-/* 
- * Script loops indefinitely asking about purchasing an elephant
- * without any regard to what the user inputs. This was our first
- * attempt at using PowerShell.
- */
+<#
+    Script loops indefinitely asking about purchasing an elephant
+    without any regard to what the user inputs. This was our first
+    attempt at using PowerShell.
+#>
 
 $foo = "Wanna buy an elephant?"
 $bar = "(Yes/No)"
