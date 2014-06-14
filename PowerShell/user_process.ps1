621377120eeebbde2813f080ed55f46cfd700b42
Üllar <yates@protonmail.ch>
2014-06-14 16:14:29 +0300

    Updated comment

--- PowerShell/user_process.ps1
+++ PowerShell/user_process.ps1
@@ -1,8 +1,8 @@
-﻿/*
- * Script takes a domain name and user name as a single argument, and
- * returns the list of processes belonging to that certain set. Can also
- * be left blank and then the current user will be used.
- */
+<#
+    Script takes a domain name and user name as a single argument, and
+    returns the list of processes belonging to that certain set. Can also
+    be left blank and then the current user will be used.
+#>
  
 param($user = "$env:USERDOMAIN\$env:USERNAME")
 Get-Process -IncludeUserName | ? { $_.UserName -eq $user } | Format-Table
