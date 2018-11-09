014c71ef7cfd4e74a3833277361d64d5a2bd106c
Üllar <yates@protonmail.ch>
2018-11-09 14:11:58 +0200

    Add 'Find-Palindrome.ps1'
    Shortest possible implementation (for me, at the moment) of finding a palindrome

---
+++ PowerShell/Find-Palindrome.ps1
@@ -0,0 +1,9 @@
+﻿ForEach ($Word in $(Get-Content ~\words.txt)) {
+    $Drow = $Word.ToLower() -split ''
+    [Array]::Reverse($Drow)
+    $Drow = $Drow -join ''
+
+    If ($Word.ToLower() -eq $Drow) {
+        Write-Output "$Word is a palindrome"
+    }
+}
